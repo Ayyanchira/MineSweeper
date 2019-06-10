718a8a9840e8ccb56e481a7c0dc025434c396cfb
Akshay Ayyanchira <ayyanchira.akshay@gmail.com>
2019-06-10 01:47:24 -0400

    Initial Commit

---
+++ MineSweeper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MineSweeper
+//
+//  Created by Akshay Ayyanchira on 6/10/19.
+//  Copyright © 2019 Akshay Ayyanchira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
